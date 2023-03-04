137d58f4ed07d6ecbb2c96496a81f69876eee281
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-04 23:57:44 +0100

    update

--- pages/Catalog.tsx
+++ pages/Catalog.tsx
@@ -1,9 +1,11 @@
 import { Box, Heading } from "@chakra-ui/react";
+import Image from "next/image";
 
 const Catalog = () => {
   return ( 
     <Box>
       <Heading>Our Product Offerings.</Heading>
+      <Box>ftyufuc</Box>
     </Box>
    );
 }
