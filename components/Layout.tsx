42b5120101379f5c7b50a77d3ace4ac2163c850c
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-06 20:48:18 +0100

    update layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,15 @@
 import { Box } from "@chakra-ui/react";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = ({children}) => {
   return ( 
     <Box>
       <Navbar />
-      
+      <main>
+        { children }
+      </main>
+      <Footer />
     </Box>
    );
 }
