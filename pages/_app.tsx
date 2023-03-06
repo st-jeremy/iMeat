cd06e30ef692cf44cb4fd18a97ad6b9fba618f7e
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-06 23:24:12 +0100

    update fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout';
 import { DynaPuff } from '@next/font/google';
 
+const dynaPuff = DynaPuff({subsets: ['latin']})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Layout>
-        <main>
+        <main className= { dynaPuff.className }>
           <Component {...pageProps} />
         </main>
       </Layout>
